8a2085531c9d22d5968a9c7e8d06bcfae6d34688
Alexander <alexanderm.kodehode@gmail.com>
2025-08-28 13:03:43 +0200

    Initial API using 7timer

---
+++ app.js
@@ -0,0 +1,9 @@
+
+// Current Weather API Endpoint
+
+const url = "http://www.7timer.info/bin/api.pl?lon=5.73&lat=58.97&product=civil&output=json";
+// optional parameters:
+
+    &units=m
+    &language=en
+    &callback=MY_CALLBACK
